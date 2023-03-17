aeedc508016ba095570a1bdfcf10824ee330513b
cassyel <cassyel1967@gmail.com>
2023-03-16 23:56:49 -0300

    Add Mocks API

---
+++ src/mocks/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    _id: '63fead9d239f6fc1ca92b54c',
+    name: 'Pizza',
+    icon: '🍕',
+  },
+  {
+    _id: '63feada9239f6fc1ca92b54f',
+    name: 'Bebidas',
+    icon: '🍻',
+  },
+  {
+    _id: '63feadb4239f6fc1ca92b552',
+    name: 'Burgers',
+    icon: '🍔',
+  },
+  {
+    _id: '63feadc0239f6fc1ca92b555',
+    name: 'Promoções',
+    icon: '🏷',
+  },
+];
---
+++ src/mocks/products.ts
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    _id: '6372e040f52e37ef85fe2c5e',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1678765137928-quatro-queijos.png',
+    price: 40,
+    category: '63fead9d239f6fc1ca92b54c',
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c5f'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c60'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c61'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c62'
+      }
+    ],
+  },
+  {
+    _id: '6372e276a381106c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    category: '63feada9239f6fc1ca92b54f',
+    imagePath: '1668473462705-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+];
