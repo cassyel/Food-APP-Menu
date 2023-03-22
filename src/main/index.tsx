1b8643a61733f5499edea74e15473e70914d9194
cassyel <cassyel1967@gmail.com>
2023-03-22 01:24:20 -0300

    Main File

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,10 +1,45 @@
-import { Container } from './styles';
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  FooterContainer,
+}from './styles';
+
 import { Header } from '../components/Header';
+import { Categories } from '../components/Categories/';
+import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
+import { Fragment } from 'react';
+import { SafeAreaView } from 'react-native';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <Fragment>
+
+      <Container>
+        <Header />
+
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+
+        <FooterContainer>
+          <Button
+            color='#fff'
+            alignBottom
+            onPress={() => alert('Novo Pedido')}>
+            Novo Pedido
+          </Button>
+        </FooterContainer>
+      </Container>
+
+      <SafeAreaView
+        style={{ backgroundColor: '#fff' }}
+      />
+    </Fragment>
   );
 }
--- src/main/styles.ts
+++ src/main/styles.ts
@@ -1,8 +1,25 @@
 import styled from 'styled-components/native';
-import { Platform, StatusBar } from 'react-native';
-
-const isAndroid = Platform.OS === 'android';
+import { StatusBar } from 'react-native';
+import { isAndroid } from '../utils/isAndroid';
 
 export const Container = styled.SafeAreaView`
-  margin-top: ${isAndroid ? `${Number(StatusBar.currentHeight) + 10}px` : '0'};
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  flex: 1;
+  background: #fafafa;
 `;
+
+export const CategoriesContainer = styled.View`
+  height: 73px;
+  margin-top: 34px;
+  align-items: center;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+
+export const FooterContainer = styled.View`
+  min-height: 110px;
+  background-color: #ffffff;
+  padding: 16px;
+` ;
